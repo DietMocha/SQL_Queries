4420d6655c3f8f7bfcf504ff8d48b24c482a0cd8
Flash Gordon <njume@Flashs-MBP.fios-router.home>
2018-07-18 06:50:59 -0400

    upgraded

--- Udemy Course/top_customer.sql
+++ Udemy Course/top_customer.sql
@@ -1,8 +1,16 @@
 SELECT
-  cust.first_name, cust.last_name, count(cust.customer_id) as rent_count, sum(pay.amount) as revenue
+  	cust.first_name,
+  	cust.last_name,
+  	count(cust.customer_id) as rent_count,
+  	sum(pay.amount) as revenue
 FROM
-  customer cust, payment pay
+  	customer cust, payment pay
 WHERE
-  cust.customer_id = pay.customer_id
+  	cust.customer_id = pay.customer_id
 GROUP BY
-  cust.customer_id
+  	cust.customer_id
+ORDER BY
+  	revenue DESC
+LIMIT
+	5
+;
